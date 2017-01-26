19a4ba75ecebe621f381853bfffdad45b9966832
Paul Childs <paul.childs@thomsonreuters.com>
2017-01-26 14:17:00 -0500

    Changed ArgumentParser setup and Error Handling

--- Create_Test_File/create_test_file.py
+++ Create_Test_File/create_test_file.py
@@ -10,6 +10,32 @@ import argparse
 import logging
 import os
 
+# Gather arguments which contain desired file size and path.
+args = argparse.ArgumentParser(prog='create_test_file.py',
+                               description='This script will create a file of a specific size for testing purposes.'
+                                           ' The file size and path can be specified as options. The default file'
+                                           ' size is 5MB if a size is not provided.  The current working directory'
+                                           ' will be the default path if one is not provided. The created file will'
+                                           ' be named "test_file-XXMB.dat" where XX is the file size.')
+
+args.add_argument('--size',
+                  type=int,
+                  help='The file size in MB.',
+                  default=5,
+                  required=False)
+
+args.add_argument('--path',
+                  type=str,
+                  help='The path where the file is created.',
+                  default=os.getcwd(),
+                  required=False)
+
+command_arguments = args.parse_args()
+
+file_size_in_mb = command_arguments.size
+dir_path = command_arguments.path
+file_name = "test_file-{0}MB.dat".format(str(file_size_in_mb))
+
 # Setup logging.  Display INFO messages and higher to console
 logger = logging.getLogger(__name__)
 log_handler = logging.StreamHandler()
@@ -24,21 +50,6 @@ def main():
     of a particular size.
     """
 
-    args = argparse.ArgumentParser(prog='create_test_file.py',
-                                   description='This script will create a file of a specific size for testing purposes.'
-                                               ' The file size and path can be specified as options. The default file'
-                                               ' size is 5MB if a size is not provided.  The current working directory'
-                                               ' will be the default path if one is not provided. The created file will'
-                                               ' be named "test_file-XXMB.dat" where XX is the file size.')
-    args.add_argument('-s', type=int, help='The file size in MB.', default=5, required=False)
-    args.add_argument('-p', type=str, help='The path where the file is created.', default=os.getcwd(), required=False)
-
-    command_arguments = args.parse_args()
-
-    file_size_in_mb = command_arguments.s
-    dir_path = command_arguments.p
-    file_name = "test_file-{0}MB.dat".format(str(file_size_in_mb))
-
     logger.info("Starting To Build File")
 
     try:
@@ -46,7 +57,13 @@ def main():
         with open(dir_path + "\\" + file_name, "wb") as fh:
             fh.truncate(1024*1024*file_size_in_mb)
 
-    except Exception:
+    except IOError as e:
+
+        logger.exception('Unable to Open or Write to File')
+
+        logger.exception(e)
+
+    except Exception as e:
 
         logger.exception("Failure In Building File")
         quit()
